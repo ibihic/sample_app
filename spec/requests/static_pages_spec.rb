d6b83504c5cea691cfea34875b614740e508ae57
xyz <ibihic@ibihic.com>
2013-10-21 16:47:13 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,33 +2,25 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject {page}
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Sample App")
-    end
-    
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title (' | Home') }
   end
 
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Sample App | Help")
     end
   end
@@ -36,12 +28,12 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Sample App | About Us")
     end
   end
@@ -49,12 +41,12 @@ describe "Static pages" do
     describe "Contact page" do
 
     it "should have the content 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact Us')
     end
 
     it "should have the right title 'Contact Us'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Sample App | Contact Us")
     end
   end
