63ecffc8839222359b4aea877b35fc5fbb6f324d
xyz <ibihic@ibihic.com>
2013-10-30 11:16:11 -0700

    Finish user edit, index, update, destroy actions with some error

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,9 +14,21 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = " " }
@@ -86,6 +98,13 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when confirmation is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
@@ -110,6 +129,11 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
 
